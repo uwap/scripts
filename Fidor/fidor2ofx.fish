190e800caecf8bd82ee091491c110717a51ea49c
uwap <me@uwap.name>
2019-06-16 20:19:27 +0200

    Add payees to fidor2qif; drop fidor2ofx

--- Fidor/fidor2ofx.fish
+++
@@ -1,9 +0,0 @@
-#!/bin/env fish
-
-# This converts a CVS in the format the fidor bank uses
-# and converts it to QIF using fidor2qif and then
-# uses https://github.com/wesabe/fixofx to convert it
-# to OFX.
-
-. ~/Projekte/Skripte/fidor2qif.fish $argv[1] /tmp/2ofxtmp
-/bin/env python2 fixofx.py -f /tmp/2ofxtmp --curdef=EUR --lang=DEU --dayfirst > $argv[2]
