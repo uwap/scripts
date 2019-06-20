961f53cf9a1e27c0a5fd77c3212c2c74b2f6eb72
uwap <me@uwap.name>
2019-06-20 21:09:29 +0200

    fidor2qif now works without the second parameter

--- Fidor/fidor2qif.fish
+++ Fidor/fidor2qif.fish
@@ -6,8 +6,15 @@
 # The input file shall be a CSV in the format fidor bank delivers it.
 # The Output is a QIF file that works with gnucash and ynab.
 
-echo "!Type:Bank" > $argv[2]
-tail -n +2 $argv[1] \
+set csvfile $argv[1]
+if set -q argv[2]
+  set qiffile $argv[2]
+else
+  set qiffile (string join "." (string split -r -m1 . $csvfile)[1] "qif")
+end
+
+echo "!Type:Bank" > $qiffile
+tail -n +2 $csvfile \
  # | uconv -f ISO-8859-1 \
   | sed -r 's/(.*);(.*);(.*);(.*)/D\1\nM\2\nP\3\nT\4\n\^/;s/""//' \
-  | string trim >> $argv[2]
+  | string trim >> $qiffile
